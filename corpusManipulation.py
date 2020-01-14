405f4131903528721e81ce6d51f22e1b7de85396
Adèle Richard <richaradele@gmail.com>
2020-01-14 12:20:57 +0100

    Score bleu method added

--- corpusManipulation.py
+++ corpusManipulation.py
@@ -1,31 +1,32 @@
 import json
 from collections import defaultdict
-import math
+from math import *
+import nltk
+import nltk.translate.bleu_score as bleu
+import warnings
+warnings.filterwarnings('ignore')
 
 def readText(filename) :
+    #fileDict = defaultdict()
     with open(filename, 'r', encoding="utf8") as file :
         fileDict = json.load(file)
-
-    """ for key in fileDict :
-        print(key["src_lang"]) """
 
     # corpus est une liste de dictionnaires
     # les cles du dictionnaires permettent d
     # identifier la nature de chaque information
     return fileDict
 
-#Affiche le nombres de langues 
+#Affiche le nombres de direction de traduction 
 def countLang(corpusDict) :
 
-    listLang = set()
-
+    dicoTrad = defaultdict(float)
+    
     for key in corpusDict :
-        """ if key["src_lang"] not in listLang :
-            listLang.append()
-        print(key) """
-        listLang.add(key["src_lang"])
+        dicoTrad[key["src_lang"], key["tgt_lang"]] += 1.0
 
-    print(len(listLang))
+
+    print(len(dict(dicoTrad).keys()))
+    return dicoTrad
 
 #Compte le nombre d'exemples présents dans le corpus
 def countExem(corpusDict) :
@@ -35,29 +36,85 @@ def countExem(corpusDict) :
         if key["hyp"] :
             nbOfExem += 1
 
-    print(nbOfExem)
+    return nbOfExem
 
 #Evalue les scores moyens des traductions
 def countScore(corpusDict) :
     
     scoreDict = defaultdict(float)
-    total = 0
+    totalFrom = 0.0
+    totalTo = 0.0
 
     for key in corpusDict :
-        if key["score"] and key["orig_lang"] == "en" :
+        if key["score"] and key["tgt_lang"] == "en" :
             scoreDict["toEn"] += key["score"]
-            total += 1
+            totalTo += 1.0
         elif key["score"] and key["src_lang"] == "en" :
             scoreDict["fromEn"] += key["score"]
-            total += 1
+            totalFrom += 1.0
 
-    print(scoreDict["fromEn"]/ total)
-    print(scoreDict["toEn"]/ total)
+    print(scoreDict["fromEn"] / totalFrom)
+    print(scoreDict["toEn"] / totalTo)
 
+    print(totalFrom * 100 / countExem(corpusDict))
+    print(totalTo * 100 / countExem(corpusDict))
+
+def constructDicoBleu(dicoCorpus, directTrad, dicoTrad) :
+    """
+        Méthode qui construit le dictionnaire de chaque direction 
+        de traduction
+        key : phrase ref, value : liste phrase hypothèses
+    """
+    listeHypoth = set()
+    for key in corpusDict :
+        if key['src_lang'] == directTrad and key['tgt_lang'] == directTrad :
+            listeHypoth.add(key['hyp'])
+            dicoTrad[key['src']] = listeHypoth
+    
+    return dicoTrad
+
+
+
+def scoreBleu(corpusDict, dicoTrad) :
+    """
+        Méthode qui calcule le score bleu
+        de chaque direction de traduction
+    """
+
+    listeScore = []
+    for directTrad in dicoTrad :
+        listeHyp = []
+        listeRef = []
+        #print(directTrad)
+        for key in corpusDict :
+            if key['src_lang'] in directTrad and key['tgt_lang'] in directTrad :
+                ref = key['ref']
+                hyp = key['hyp']
+                """ print(ref.split())
+                print(hyp.split()) """
+                listeRef.append(ref.split())
+                listeHyp.append(hyp.split())
+            
+            #listeScore.append(bleu.sentence_bleu(ref.split(), hyp.split()))
+        """
+            NOTE: Pour chaque ref qui est la même -> faire une liste de toutes les hypothèses ?
+            Et faire sentence_bleu() et additionner tous les scores ?
+            Faire dictionnaire, phrase src : liste de hypothèses
+        """ 
+         
+        #break 
+        
+        """ print(listeRef)
+        print(listeHyp) """
+        #break
+        dicoTrad[directTrad] = bleu.corpus_bleu(listeRef, listeHyp)
+    #print(listeScore)
+    print(dicoTrad)
     
 
 if __name__ == "__main__":
     corpusDict = readText("da_newstest2016.json")
-    countLang(corpusDict)
+    dicoTrad = countLang(corpusDict)
     countExem(corpusDict)
     countScore(corpusDict)
+    scoreBleu(corpusDict, dicoTrad)
